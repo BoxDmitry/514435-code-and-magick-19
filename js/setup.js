8bdd74ead6cb5e5facc563b74009aa01592968ab
Dmitry-407 <52531675+Dmitry-407@users.noreply.github.com>
2020-01-25 13:54:05 +0300

    Исправление замечаний Travis

--- js/setup.js
+++ js/setup.js
@@ -1,9 +1,15 @@
-//'use strict';
-//
+'use strict';
+
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var getRandom = function (min, max) {
+  var least = Math.ceil(min);
+  var most = Math.floor(max);
+  return Math.floor(Math.random() * (most - least + 1)) + least;
+};
 
 var settingsWindow = document.querySelector('.setup');
 settingsWindow.classList.remove('hidden');
