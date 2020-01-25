4a5ca2c1289875a2bc40b5f57745e28ce83cf762
Dmitry-407 <52531675+Dmitry-407@users.noreply.github.com>
2020-01-25 09:17:11 +0300

    Исправление замечаний

--- js/stat.js
+++ js/stat.js
@@ -2,7 +2,7 @@
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
-var SHADOW_OFFESET = 10;
+var SHADOW_OFFSET = 10;
 var CLOUD_X = 125;
 var CLOUD_Y = 45;
 var GAP = 20;
@@ -24,16 +24,16 @@ var getMaxElement = function(arr) {
 
 var printCloud = function (ctx, x, y, color, colorShadow) {
   ctx.fillStyle = colorShadow;
-  ctx.fillRect(x + SHADOW_OFFESET, y + SHADOW_OFFESET, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillRect(x + SHADOW_OFFSET, y + SHADOW_OFFSET, CLOUD_WIDTH, CLOUD_HEIGHT);
 
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 }
 
 function getRandom (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  var least = Math.ceil(min);
+  var most = Math.floor(max);
+  return Math.floor(Math.random() * (most - least + 1)) + least;
 }
 
 window.renderStatistics = function (ctx, players, times) {
@@ -46,17 +46,13 @@ window.renderStatistics = function (ctx, players, times) {
 
   var maxTime = getMaxElement(times);
 
-  console.log(times);
-
   for (var i = 0; i < players.length; i++) {
     ctx.fillStyle = '#000';
     ctx.fillText(Math.floor(times[i]), (CHART_MARGIN + CHART_WIDTH) * i + CLOUD_X + CLOUD_X/4.5, CLOUD_Y + GAP * 2.3 + CHART_HEIGHT - (CHART_HEIGHT * times[i]) / maxTime);
 
     ctx.fillStyle = '#000';
     ctx.fillText(players[i], (CHART_MARGIN + CHART_WIDTH) * i + CLOUD_X + CLOUD_X/4.5, CLOUD_Y + GAP * 3.5 + CHART_HEIGHT);
-    var color = players[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(240, 100%, '+ random(10, 100) +'%)';
-
-    console.log(color);
+    var color = players[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(240, 100%, '+ getRandom(10, 100) +'%)';
 
     ctx.fillStyle = color;
     ctx.fillRect((CHART_MARGIN + CHART_WIDTH) * i + CLOUD_X + CLOUD_X/4.5, CLOUD_Y + GAP * 2.5 + CHART_HEIGHT - (CHART_HEIGHT * times[i]) / maxTime, CHART_WIDTH, (CHART_HEIGHT * times[i]) / maxTime);
