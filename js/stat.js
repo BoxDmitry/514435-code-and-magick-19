f6a94f1c323b7f9ab3a05fccd1b279f660cdfea1
Dmitry-407 <52531675+Dmitry-407@users.noreply.github.com>
2020-01-25 09:25:36 +0300

    Исправление замечений

--- js/stat.js
+++ js/stat.js
@@ -10,7 +10,7 @@ var CHART_HEIGHT = 150;
 var CHART_WIDTH = 40;
 var CHART_MARGIN = 50;
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 0; i < arr.length; i++) {
@@ -28,13 +28,13 @@ var printCloud = function (ctx, x, y, color, colorShadow) {
 
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-}
+};
 
 function getRandom (min, max) {
   var least = Math.ceil(min);
   var most = Math.floor(max);
   return Math.floor(Math.random() * (most - least + 1)) + least;
-}
+};
 
 window.renderStatistics = function (ctx, players, times) {
   printCloud(ctx, 100, 10, '#fff', 'rgba(0, 0, 0, 0.7)');
@@ -48,13 +48,13 @@ window.renderStatistics = function (ctx, players, times) {
 
   for (var i = 0; i < players.length; i++) {
     ctx.fillStyle = '#000';
-    ctx.fillText(Math.floor(times[i]), (CHART_MARGIN + CHART_WIDTH) * i + CLOUD_X + CLOUD_X/4.5, CLOUD_Y + GAP * 2.3 + CHART_HEIGHT - (CHART_HEIGHT * times[i]) / maxTime);
+    ctx.fillText(Math.floor(times[i]), (CHART_MARGIN + CHART_WIDTH) * i + CLOUD_X + CLOUD_X / 4.5, CLOUD_Y + GAP * 2.3 + CHART_HEIGHT - (CHART_HEIGHT * times[i]) / maxTime);
 
     ctx.fillStyle = '#000';
-    ctx.fillText(players[i], (CHART_MARGIN + CHART_WIDTH) * i + CLOUD_X + CLOUD_X/4.5, CLOUD_Y + GAP * 3.5 + CHART_HEIGHT);
-    var color = players[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(240, 100%, '+ getRandom(10, 100) +'%)';
+    ctx.fillText(players[i], (CHART_MARGIN + CHART_WIDTH) * i + CLOUD_X + CLOUD_X / 4.5, CLOUD_Y + GAP * 3.5 + CHART_HEIGHT);
+    var color = players[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(240, 100%, ' + getRandom(10, 100) + '%)';
 
     ctx.fillStyle = color;
-    ctx.fillRect((CHART_MARGIN + CHART_WIDTH) * i + CLOUD_X + CLOUD_X/4.5, CLOUD_Y + GAP * 2.5 + CHART_HEIGHT - (CHART_HEIGHT * times[i]) / maxTime, CHART_WIDTH, (CHART_HEIGHT * times[i]) / maxTime);
+    ctx.fillRect((CHART_MARGIN + CHART_WIDTH) * i + CLOUD_X + CLOUD_X / 4.5, CLOUD_Y + GAP * 2.5 + CHART_HEIGHT - (CHART_HEIGHT * times[i]) / maxTime, CHART_WIDTH, (CHART_HEIGHT * times[i]) / maxTime);
   }
 }
