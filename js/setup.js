40c628a492537d98d4d33dd59e3fa24e61f8cb56
Dmitry-407 <52531675+Dmitry-407@users.noreply.github.com>
2020-01-27 18:05:41 +0300

    Исправление замечаний

--- js/setup.js
+++ js/setup.js
@@ -1,15 +1,23 @@
 'use strict';
 
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var WIZARD_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var wizardsQuantity = 4
 
 var getRandom = function (min, max) {
   var least = Math.ceil(min);
   var most = Math.floor(max);
   return Math.floor(Math.random() * (most - least + 1)) + least;
 };
+
+var getRandomElement = function (array) {
+  var min = 0;
+  var max = array.length - 1;
+  return array[getRandom(min, max)];
+}
 
 var settingsWindow = document.querySelector('.setup');
 settingsWindow.classList.remove('hidden');
@@ -20,14 +28,18 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template').c
 
 var generationWizard = function () {
   var wizardОbject = {
-    name: WIZARD_NAMES[getRandom(0, WIZARD_NAMES.length - 1)] + ' ' + WIZARD_SURNAME[getRandom(0, WIZARD_SURNAME.length - 1)],
-    coatColor: WIZARD_COAT_COLOR[getRandom(0, WIZARD_COAT_COLOR.length - 1)],
-    eyesColor: WIZARD_EYES_COLOR[getRandom(0, WIZARD_EYES_COLOR.length - 1)]
+    name: getRandomElement(WIZARD_NAMES) + ' ' + getRandomElement(WIZARD_SURNAMES),
+    coatColor: getRandomElement(WIZARD_COAT_COLORS),
+    eyesColor: getRandomElement(WIZARD_EYES_COLORS)
   };
   return wizardОbject;
 };
 
-var wizards = [generationWizard(), generationWizard(), generationWizard(), generationWizard()];
+var wizards = [];
+
+for (var i = 0; i < wizardsQuantity; i++) {
+  wizards[i] = generationWizard();
+}
 
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
