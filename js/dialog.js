3d0d3d6617b2c502c6ff32efc8145100f07aac44
Dmitry-407 <52531675+Dmitry-407@users.noreply.github.com>
2020-02-14 20:17:52 +0300

    Учебный проект: pimp my mage

--- js/dialog.js
+++ js/dialog.js
@@ -14,6 +14,9 @@
   var wizardFairbollInput = document.querySelector('input[name="fireball-color"]');
   var nameInput = document.querySelector('.setup-user-name');
 
+  var form = document.querySelector('.setup-wizard-form');
+  var sendForm = document.querySelector('.setup-submit');
+
   var openPopup = function () {
     settingsWindow.classList.remove('hidden');
     document.addEventListener('keydown', onPopupEscPress);
@@ -43,19 +46,19 @@
   };
 
   var onWizardEyesColor = function () {
-    var color = window.random.getElement(window.constants.WIZARD_EYES_COLORS);
+    var color = window.util.getRandomElement(window.constants.WIZARD_EYES_COLORS);
     wizardEyesBlock.style.fill = color;
     wizardEyesInput.value = color;
   };
 
   var onWizardCoatColor = function () {
-    var color = window.random.getElement(window.constants.WIZARD_COAT_COLORS);
+    var color = window.util.getRandomElement(window.constants.WIZARD_COAT_COLORS);
     wizardCoatBlock.style.fill = color;
     wizardCoatInput.value = color;
   };
 
   var onWizardFairbollColor = function () {
-    var color = window.random.getElement(window.constants.WIZARD_FAIRBOLL_COLORS);
+    var color = window.util.getRandomElement(window.constants.WIZARD_FAIRBOLL_COLORS);
     wizardFairbollBlock.style.backgroundColor = color;
     wizardFairbollInput.value = color;
   };
@@ -69,6 +72,30 @@
   var onBlurInput = function () {
     statFocusInput = true;
   };
+
+  var successHandler = function (wizards) {
+    closePopup();
+  };
+
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red; padding: 20px; border-radius: 10px';
+    node.style.position = 'absolute';
+    node.style.left = '50%';
+    node.style.top = 0;
+    node.style.transform = 'translateX(-50%) translateY(50%)';
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  var onSendForm = function (evt) {
+    evt.preventDefault();
+
+    var urlSendForm = 'https://js.dump.academy/code-and-magick';
+    window.backend.save(successHandler, errorHandler, urlSendForm, new FormData(form));
+  }
 
   buttonOpenSetting.addEventListener('click', function () {
     openPopup();
@@ -87,6 +114,14 @@
   buttonClossSetting.addEventListener('keydown', function (evt) {
     if (evt.key === window.constants.ENTER_KEY) {
       closePopup();
+    }
+  });
+
+  sendForm.addEventListener('click', onSendForm);
+
+  sendForm.addEventListener('keydown', function (evt) {
+    if (evt.key === window.constants.ENTER_KEY) {
+      onSendForm();
     }
   });
 })();
