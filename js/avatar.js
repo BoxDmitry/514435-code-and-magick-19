5d4e8c5f3d7b9ddb0b92244f29be3ae8e453bed2
Dmitry-407 <52531675+Dmitry-407@users.noreply.github.com>
2020-02-19 16:01:09 +0300

    Учебный проект: выкладываем фото

--- js/avatar.js
+++ js/avatar.js
@@ -3,16 +3,14 @@
 (function () {
   var fileChooser = document.querySelector('.upload input[type=file]');
   var preview = document.querySelector('.setup-user-pic');
-  var url;
 
   fileChooser.addEventListener('change', function () {
     var file = fileChooser.files[0];
-    var fileName = file.name.toLowerCase();
 
     var reader = new FileReader();
 
     reader.addEventListener('load', function () {
-      url = reader.result;
+      var url = reader.result;
       preview.src = url;
 
       window.avatar = {
