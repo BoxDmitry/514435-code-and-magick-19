346c3fcfd4f9e254aef4709c536de097bd415c97
Dmitry-407 <52531675+Dmitry-407@users.noreply.github.com>
2020-02-20 22:37:29 +0300

    Учебный проект: выкладываем фото

--- js/avatar.js
+++ js/avatar.js
@@ -8,46 +8,50 @@
 
   fileChooser.addEventListener('change', function () {
     var file = fileChooser.files[0];
-    if (file) {
-      var fileName = file.name.toLowerCase();
 
-      var matches = FILE_TYPES.some(function (it) {
-        return fileName.endsWith(it);
+    if (!file) return;
+
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        var url = reader.result;
+        preview.src = url;
+
+        window.avatar = {
+          url: url
+        };
       });
 
-      if (matches) {
-        var reader = new FileReader();
+      reader.readAsDataURL(file);
 
-        reader.addEventListener('load', function () {
-          var url = reader.result;
-          preview.src = url;
-
-          window.avatar = {
-            url: url
-          };
-        });
-
-        reader.readAsDataURL(file);
-      } else {
-        var node = document.createElement('div');
-        node.classList.add('error--popap');
-        node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red; padding: 20px; border-radius: 10px';
-        node.style.position = 'absolute';
-        node.style.left = '50%';
-        node.style.top = 0;
-        node.style.transform = 'translateX(-50%) translateY(50%)';
-        node.style.fontSize = '30px';
-
-        node.textContent = 'Не допустимый формат файла';
-        document.body.insertAdjacentElement('afterbegin', node);
-
-        var onHiddenError = function () {
-          document.querySelector('.error--popap').removeEventListener('click', onHiddenError);
-          document.querySelector('.error--popap').remove();
-        };
-
-        document.querySelector('.error--popap').addEventListener('click', onHiddenError);
-      }
+      return;
     }
+
+    var node = document.createElement('div');
+    node.classList.add('error--popap');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red; padding: 20px; border-radius: 10px';
+    node.style.position = 'absolute';
+    node.style.left = '50%';
+    node.style.top = 0;
+    node.style.transform = 'translateX(-50%) translateY(50%)';
+    node.style.fontSize = '30px';
+
+    node.textContent = 'Не допустимый формат файла';
+    document.body.insertAdjacentElement('afterbegin', node);
+
+    var onHiddenError = function () {
+      document.querySelector('.error--popap').removeEventListener('click', onHiddenError);
+      document.querySelector('.error--popap').remove();
+    };
+
+    document.querySelector('.error--popap').addEventListener('click', onHiddenError);
   });
 })();
